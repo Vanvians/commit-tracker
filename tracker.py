d2a200e861d3b25b495ba5157d59d8c3ce2c97a7
Vanvians <jcezetah@gmail.com>
2025-12-06 02:45:58 +0300

    Added simple  main function

--- tracker.py
+++ tracker.py
@@ -1,4 +1,12 @@
 #Main
+if _name__ == "__main__":
+    repo_path = input(" Please enter the path to your Git repository here:").strip()
+
+    output_1(repo_path)
+    output_2(repo_path)
+
+    print("Analysis complete. Results saved to output_1.txt and output_2.csv")
+
 
 #Run Git
 
