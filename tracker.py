4549b2d9a3bd4bc851a6e61100e7cc7c745248e0
Vanvians <jcezetah@gmail.com>
2025-12-06 02:17:43 +0300

    Initial commit contains work plan

---
+++ tracker.py
@@ -0,0 +1,19 @@
+#Main
+
+#Run Git
+
+#collect commit hashes
+
+#List all files in a commit
+
+#Find added/deleted/modified files
+
+#Get list of all commits that modified a single file
+
+#Compute churn 
+
+#Count unuique contributors to a file
+
+#Generate output_1.txt
+
+#Generate output_2.csv
